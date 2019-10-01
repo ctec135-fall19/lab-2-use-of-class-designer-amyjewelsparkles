3a66f6ce64e2edbf50f94082160ba5d8f46e8aee
a.khan <a.khan4@students.clark.edu>
2019-10-01 15:16:39 -0700

    Complete lab 2

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up Console UI (CUI)
+            Console.Title = "Hello World!!!";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("********** Hello World! *************");
+            Console.WriteLine("*************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // Display a Car name on screen
+
+            Console.WriteLine("Hello World");
+            
+            // Wait for Enter key to be pressed.
+            Console.ReadLine();
+
+        }
+    }
+}
